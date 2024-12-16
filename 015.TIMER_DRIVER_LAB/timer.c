91ef508293561cc95fae23635bf684bd32efbf21
DaeHunJang <huni9603@gmail.com>
2024-12-16 17:05:25 +0900

    Third commit

--- 015.TIMER_DRIVER_LAB/timer.c
+++ 015.TIMER_DRIVER_LAB/timer.c
@@ -5,22 +5,28 @@ void TIM2_Stopwatch_Start(void)
 	Macro_Set_Bit(RCC->APB1ENR, 0);
 
 	// TIM2 CR1 설정: down count, one pulse
+	TIM2->CR1 = (TIM2->CR1 & ~((0x1f << 5) | (0x3 << 1))) | ((1 << 4) | (1 << 3));
 	// PSC 초기값 설정 => 20usec tick이 되도록 설계 (50KHz)
+	TIM2->PSC = (72000000 / 50000) - 1;
 	// ARR 초기값 설정 => 최대값 0xFFFF 설정
+	TIM2->ARR = 0xffff;
 	// UG 이벤트 발생
+	Macro_Set_Bit(TIM2->EGR,0);
 	// TIM2 start
-
+	TIM2->CR1 |= (1 << 0);
 }
 
 unsigned int TIM2_Stopwatch_Stop(void)
 {
 	unsigned int time;
-
 	// TIM2 stop
+	TIM2->CR1 &= ~(1 << 0);
 	// CNT 초기 설정값 (0xffff)와 현재 CNT의 펄스수 차이를 구하고
+	time = TIM2->ARR - TIM2->CNT;
 	// 그 펄스수 하나가 20usec이므로 20을 곱한값을 time에 저장
+	time = time * 20;
 	// 계산된 time 값을 리턴(단위는 usec)
-
+	return time;
 }
 
 void TIM2_Delay(int time)
@@ -63,15 +69,11 @@ void TIM4_Repeat(int time)
 	// Update Interrupt Enable
 
 	// TIM4 start
-
 }
 
 int TIM4_Check_Timeout(void)
 {
 	// 타이머가 timeout 이면 1 리턴, 아니면 0 리턴
-
-
-
 }
 
 void TIM4_Stop(void)
